bcc7295e8aa470771f5e975dbff78cb8ec50db63
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-01 13:28:55 +0400

    pagination-products

--- controllers/products.js
+++ controllers/products.js
@@ -7,17 +7,24 @@ const getAllProductsStatic = async (req,res) =>{
 }
 
 const getAllProducts = async (req,res) =>{
-    const {feature , name,sort } = req.query
+    const {feature,name,sort } = req.query
     const queryObject = {}
     if(feature)   queryObject.feature = feature === 'true' ? true : false
     if(name)   queryObject.name = {$regex : name , $options :'i'}
-    
 
     let result =  Product.find(queryObject)
+    
     if(sort){
       const sortList = sort.split(',').join(' ')
       result = result.sort(sortList)
     }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1) * limit
+
+    result = result.skip(skip).limit(limit)
+
+
     const products = await result
     res.status(200).json({products})
 
