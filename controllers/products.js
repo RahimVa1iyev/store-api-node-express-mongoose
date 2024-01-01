c611deba7c57a43e33502f96d6d7f8e98026bb52
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-01 15:22:25 +0400

    filter-by-price

--- controllers/products.js
+++ controllers/products.js
@@ -7,13 +7,15 @@ const getAllProductsStatic = async (req,res) =>{
 }
 
 const getAllProducts = async (req,res) =>{
-    const {feature,name,sort } = req.query
+    const {feature,name,sort,max,min } = req.query
     const queryObject = {}
     if(feature)   queryObject.feature = feature === 'true' ? true : false
-    if(name)   queryObject.name = {$regex : name , $options :'i'}
+    if(name) queryObject.name = {$regex : name , $options :'i'}
+    if (max) queryObject.price = { ...queryObject.price, $lt: max }; 
+    if (min) queryObject.price = { ...queryObject.price, $gt: min };
 
     let result =  Product.find(queryObject)
-    
+
     if(sort){
       const sortList = sort.split(',').join(' ')
       result = result.sort(sortList)
@@ -23,6 +25,7 @@ const getAllProducts = async (req,res) =>{
     const skip = (page-1) * limit
 
     result = result.skip(skip).limit(limit)
+
 
 
     const products = await result
